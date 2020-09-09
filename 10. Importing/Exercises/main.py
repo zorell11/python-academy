d3b765d78c734e6c3ec13c0340492fa89fb473df
zorell11 <zorell@azet.sk>
2020-09-09 10:01:26 +0200

    import lesson

---
+++ 10. Importing/Exercises/main.py
@@ -0,0 +1,7 @@
+import test
+a, b = 100, 100
+print(test.func(a,b))
+
+
+from test import func
+print(func(a, b))
---
+++ 10. Importing/Exercises/test.py
@@ -0,0 +1,10 @@
+#print('Print is being executed')
+var1 = 52
+var2 = 53
+def func(v1,v2):
+    result = v1 * v2
+    return result
+
+print(func(var1, var2))
+if __name__ == "__main__":
+    print(func(var1,var2))
